9f19f72a5c2a1f22d482bb7d90e6e80dfb4d2c84
Glenn Hancock <spudhead42@hotmail.com>
2015-11-09 16:15:25 +0900

    Working but only for one guess

--- High or Low IOS/ViewController.swift
+++ High or Low IOS/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var LowTF: UITextField!
+    @IBOutlet weak var HiTF: UITextField!
+    @IBOutlet weak var GuessTF: UITextField!
+    @IBOutlet weak var MesageWindow: UITextView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,43 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func newGameBttn(sender: UIButton) {
+     
+        
+    }
+    
 
+    @IBAction func GoBttn(sender: UIButton) {
+        
+        
+      func randomIntBetween(low:Int, high:Int) -> Int {
+            let range = high - (low - 1)
+            return (Int(arc4random()) % range) + (low)
+        }
+        
+        let guess = Int(GuessTF.text!)
+        var turn = 1
+        let hi = Int(HiTF.text!)
+        let low = Int(LowTF.text!)
+        let answer = randomIntBetween(low!, high: hi!)
+        
+        //        while(guess != answer) {
+            if guess < answer {
+                MesageWindow.text = MesageWindow.text + "Higher!"
+            } else if guess > answer {
+                MesageWindow.text = MesageWindow.text + "Lower!"
+            } else {
+                MesageWindow.text = MesageWindow.text + "EXACTLY!"
+            }
+              turn++
+    }
 
+    
+   // }
 }
+
+
+
+
 
